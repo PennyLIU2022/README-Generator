5b328d534c55151e95697f6274d7fa1ad26e75f3
PennyLIU2022 <114879207+PennyLIU2022@users.noreply.github.com>
2022-12-11 23:17:05 -0500

    updated index.js and generateMarkdown.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,63 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
+const { fstat } = require('fs');
 const inquirer = require('requirer');
 const { writeFile } = require('fs').promises;
 const generateMarkdown = require('./utils/generateMarkdown');
+const licenses = ['None', 'Apache', 'AGPL', 'MIT','BSD'];
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter the project title:'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Write a description about this project:'
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'Enter the installation instructions:'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Give some information on how to use this project:'
+    },
+    {
+        type: 'input',
+        name: 'features',
+        message: 'If this project has a lot of features, list them here:'
+    },
+    {
+        type: 'input',
+        name: 'contribute',
+        message: 'Enter the contribution details here:'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Enter the test instructions here:'
+    },
+]
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Create a function to write README file
+function writeToFile(fileName, data) {
+    const fileName = "./result/readme.md";
 
-// TODO: Create a function to initialize app
-function init() {}
+    fs.writeFile(fileName, data, (err) =>
+    err? console.log(err): console.log(fileName +" is created!")
+    );
+}
+
+// Create a function to initialize app
+function init() {
+    inquirer.prompt(questions)
+    .then (answers => writeToFile(generateMarkdown(answers)))
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -8,21 +8,91 @@ function renderLicenseBadge(license) {
   return badge;
 }
 
-// TODO: Create a function that returns the license link
+// Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  
+  let licenseLink;
+
+  switch(license){
+    case 'Apache':
+      licenseLink = "https://www.apache.org/licenses/LICENSE-2.0.html";
+      break;
+    case 'AGPL':
+      licenseLink = "https://www.gnu.org/licenses/agpl-3.0.en.html";
+      break;
+    case 'MIT':
+      licenseLink = "https://mit-license.org/";
+      break;
+    case 'BSD':
+      licenseLink = "https://opensource.org/licenses/BSD-3-Clause";
+      break;
+    default:
+      licenseLink = "";
+      break;
+  }
+  return licenseLink;
 }
 
-// TODO: Create a function that returns the license section of README
+// Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  let licenseSection = "";
 
-// TODO: Create a function to generate markdown for README
+  if (license != "None"){
+    licenseSection += "## License\n";
+    licenseSection += "This section of a high-quality README file is the license. For more detailed information of the license, please visit"+renderLicenseLink(license)+"\n";
+  }
+  return licenseSection;
+}
+
+// Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  const section = ["Description", "Installation", "Usage", "License", "Badges", "Features","How to Contribute", "Tests"];
 
-`;
+  // title
+  let markdown = "# "+data.tile+"\n";
+
+  // description section
+  markdown += "## "+section[0]+"\n";
+  markdown += data.description+"\n";
+
+  // table of contents
+  markdown += "## Table of Contents"
+  for (let i=0; i<section.length; i++){
+    if (!(section[i]==="License" && data.license === "None")){
+      markdown += i +".["+ section[i]+"](#" + section[i][0].toLowerCase() + secion[i].substring(1) +")\n";
+    }
+  }
+  markdown += "\n";
+
+  // Installation section
+  markdown += "## "+section[1]+"\n";
+  markdown += data.install+"\n";
+
+  // Usage section
+  markdown += "## "+section[2]+"\n";
+  markdown += data.usage+"\n";
+
+  // License section
+  markdown += renderLicenseSection(data.license)+"\n";
+
+  // Badges section
+  markdown += "## "+section[4]+"\n";
+  markdown += renderLicenseBadge(data.license)+"\n";
+
+  // Features section
+  markdown += "## "+section[5]+"\n";
+  markdown += data.features+"\n";
+
+  // Contribute section
+  markdown += "## "+section[6]+"\n";
+  markdown += data.contribute+"\n";
+
+  // Test section
+  markdown += "## "+section[7]+"\n";
+  markdown += data.test+"\n";
+
+  return markdown;
 }
 
 module.exports = generateMarkdown;
